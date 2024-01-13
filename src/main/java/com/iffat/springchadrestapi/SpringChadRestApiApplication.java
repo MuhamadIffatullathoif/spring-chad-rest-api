5473e5f6e60389c4324c507b46616e90477cd78a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-14 05:52:49 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springchadrestapi/SpringChadRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChadRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChadRestApiApplication.class, args);
+    }
+
+}
